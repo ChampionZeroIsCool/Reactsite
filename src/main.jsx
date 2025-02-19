11224af102737def8d8e28e72fc4f2a333728ff1
Salvador Fairley <Salvador@Edwards-iMac.local>
2025-02-19 18:48:32 +1100

    layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import UploadFile from './UploadFile.jsx'
+import Layout from './Layout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App numberCount={5}/>
-    <App numberCount={8} />
-    <div className='flex-col flex'>
-      <UploadFile />
-    </div>
+    <Layout />
   </StrictMode>,
 )
